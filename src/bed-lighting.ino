0b82478139cec5836b1177d4dd9616eccbe6818d
particleflux <particleflux@gmail.com>
2017-11-12 13:04:54 +0100

    Initial commit

---
+++ src/bed-lighting.ino
@@ -0,0 +1,48 @@
+/*
+ * Project bed-lighting
+ * Description:
+ * Author:
+ * Date:
+ *
+ * PIR output on D3
+ * PIR mode repeat (H)
+ */
+
+static const int PIR_PIN = D3;
+
+SerialLogHandler logHandler;
+uint32_t startUpTimestamp;
+int pirState = LOW;             // we start, assuming no motion detected
+int pinValue = 0;
+
+void setup() {
+  Log.info("Starting setup sequence...");
+  Log.info("System version: %s", (const char*) System.version());
+  Log.info("device id: %s", (const char*) System.deviceID());
+
+  pinMode(PIR_PIN, INPUT);     // declare sensor as input
+  startUpTimestamp = millis();
+}
+
+void loop() {
+  checkMotion();
+}
+
+void checkMotion() {
+  pinValue = digitalRead(PIR_PIN);  // read input pinValue
+  if (pinValue == HIGH) {            // check if the input is HIGH
+    if (pirState == LOW) {
+      // we have just turned on
+      Log.info("Motion detected!");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+    }
+  } else {
+    if (pirState == HIGH){
+      // we have just turned of
+      Log.info("Motion ended!");
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
+  }
+}
